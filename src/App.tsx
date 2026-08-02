a3d5a78864fa5a1f4cda87a9012372cc50de7889
Jonballes <jonatan.a.ballestero@gmail.com>
2026-08-02 20:28:16 -0300

    Podes creer que no toma por una letra

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { About } from './Components/About/About'
 import { Skills } from './Components/Skills/Skills'
 import { Projects } from './Components/Projects/Projects'
 import { Contact } from './Components/Contact/Contact'
-import { Footer } from "./Components/Footer/Footer"
+import { Footer } from "./Components/Footer/F"
 
 
 function App() {
--- src/Components/Footer/F.tsx
+++ src/Components/Footer/F.tsx
@@ -1,4 +1,4 @@
-import "./Footer.css";
+import "./F.css";
 
 export function Footer() {
 

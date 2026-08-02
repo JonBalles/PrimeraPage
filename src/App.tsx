1595c5b257b0f74c6020106b7783d9addb1f9c0c
Jonballes <jonatan.a.ballestero@gmail.com>
2026-08-02 20:16:57 -0300

    Corrigiendo 3

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { About } from './Components/About/About'
 import { Skills } from './Components/Skills/Skills'
 import { Projects } from './Components/Projects/Projects'
 import { Contact } from './Components/Contact/Contact'
-import { Footer } from "./Components/Footer/footer"
+import { Footer } from "./Components/Footer/Footer"
 
 
 function App() {

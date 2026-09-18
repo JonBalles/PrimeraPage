79d5ffbd576133898643c200dcd81f6396b42ee8
Administrator <jonatan.a.ballestero@gmail.com>
2026-09-18 02:07:26 -0300

    fix error /Eden/app.tsx

--- src/Pages/Eden/App.tsx
+++ src/Pages/Eden/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Eden from "./Eden";
+import { Eden } from "./Eden";
 
 function Placeholder({ title }: { title: string }) {
   return (
